fe89466c5306ad733d3fced68e147524350c2166
Jake Sage <jakesa@zillowgroup.com>
2025-03-21 20:43:57 -0500

    Splashscreen

--- app/src/main/java/com/twotothirdpower/morkborgcharactersheet/MainActivity.kt
+++ app/src/main/java/com/twotothirdpower/morkborgcharactersheet/MainActivity.kt
@@ -1,17 +1,35 @@
 package com.twotothirdpower.morkborgcharactersheet
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeOut
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
+import com.twotothirdpower.morkborgcharactersheet.commonuiresources.OldNewspaper
 import com.twotothirdpower.morkborgcharactersheet.commonuiresources.SoothsayerTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +39,6 @@ class MainActivity : ComponentActivity() {
             SoothsayerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +48,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    var isVisible by remember { mutableStateOf(true) }
+
+    LaunchedEffect(Unit) {
+        delay(3000) // 3 seconds
+        isVisible = false
+    }
+
+    Box(modifier = modifier.fillMaxSize()) {
+        AnimatedVisibility(
+            visible = isVisible,
+            exit = fadeOut()
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .paint(
+                        painterResource(id = R.drawable.mork_borg),
+                        contentScale = ContentScale.FillBounds
+                    )
+            ) {
+                // License is required to be displayed upon app startup
+                Text(
+                    text = stringResource(R.string.license),
+                    color = Color.White,
+                    fontSize = 12.sp,
+                    fontFamily = OldNewspaper,
+                    modifier = modifier.align(Alignment.BottomCenter)
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun GreetingPreview() {
     SoothsayerTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
