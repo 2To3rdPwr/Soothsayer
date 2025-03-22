c7ddde8d21149e81461d1a6175a52d191c0cf045
Jake Sage <jakesa@zillowgroup.com>
2025-03-22 14:36:55 -0500

    Add drawable resources

--- screen/CharacterSheet/impl/src/main/java/com/twotothirdpower/morkborgcharactersheet/charactersheet/impl/CharacterSheetScreenImpl.kt
+++ screen/CharacterSheet/impl/src/main/java/com/twotothirdpower/morkborgcharactersheet/charactersheet/impl/CharacterSheetScreenImpl.kt
@@ -8,6 +8,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.twotothirdpower.morkborgcharactersheet.charactersheet.CharacterSheetScreen
 import javax.inject.Inject
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Preview
+import com.twotothirdpower.morkborgcharactersheet.commonuiresources.SoothsayerTheme
 
 class CharacterSheetScreenImpl @Inject constructor() : CharacterSheetScreen {
     @Composable
@@ -18,5 +21,25 @@ class CharacterSheetScreenImpl @Inject constructor() : CharacterSheetScreen {
         ) {
             Text(text = "Character Sheet Screen")
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CharacterSheetScreenLightPreview() {
+    SoothsayerTheme {
+        CharacterSheetScreenImpl().Content(
+            modifier = Modifier
+        )
+    }
+}
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun CharacterSheetScreenDarkPreview() {
+    SoothsayerTheme {
+        CharacterSheetScreenImpl().Content(
+            modifier = Modifier
+        )
     }
 } 
