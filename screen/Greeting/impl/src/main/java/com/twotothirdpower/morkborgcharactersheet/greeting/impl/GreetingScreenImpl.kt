6947909b9c5a2e292937ba6bc3fa8f871247592d
Jake Sage <jakesa@zillowgroup.com>
2025-03-28 16:12:55 -0500

    RememberUpdatedState for Greeting OnComplete

--- screen/Greeting/impl/src/main/java/com/twotothirdpower/morkborgcharactersheet/greeting/impl/GreetingScreenImpl.kt
+++ screen/Greeting/impl/src/main/java/com/twotothirdpower/morkborgcharactersheet/greeting/impl/GreetingScreenImpl.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
@@ -29,9 +31,11 @@ class GreetingScreenImpl @Inject constructor() : GreetingScreen {
         modifier: Modifier,
         onGreetingComplete: () -> Unit
     ) {
+        // Use rememberUpdatedState to ensure `onGreetingComplete` is called changes after the LaunchedEffect starts
+        val onComplete by rememberUpdatedState(onGreetingComplete)
         LaunchedEffect(Unit) {
             delay(3000L)
-            onGreetingComplete()
+            onComplete()
         }
 
         Box(
